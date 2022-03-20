f8a3d9c5d7096d1285d1694be5a92043990ee141
andrija.lazic <lazic.andrija223@gmail.com>
2022-03-20 21:42:47 +0100

    Dodata funkcija za citanje fajlova -moguce citanje celih fajlova -moguce citanje odredjenih delova falova u zavisnosti od izabranih argumenata

---
+++ Projekat/Projekat/Controllers/SessionControler.cs
@@ -0,0 +1,60 @@
+﻿using ChoETL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+using System.Text;
+using Projekat.Ostalo;
+namespace Projekat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionControler : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<ActionResult<string>> uploadData(IFormFile file)
+        {
+            try
+            {
+                string pom = (RadSaFajlovima.UcitajFajl("637833769309922112",0,0)).Rows[100][0].ToString();
+                if (!string.IsNullOrEmpty(pom))
+                {
+                    return Ok(pom);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (ex.Message.Contains("Could not find file"))
+                    return BadRequest("Dati fajl ne postoji");
+                
+                
+                return BadRequest("Doslo je do greske pri ucitavanju fajla");
+            }
+            
+            if (file.Length == 0)
+            {
+                return BadRequest("bad");
+            }
+            string fileName = file.FileName;
+            
+            await using var stream = file.OpenReadStream();
+
+
+            var reader = new StreamReader(stream);
+            var text = await reader.ReadToEndAsync();
+
+            StringBuilder sb = new StringBuilder();
+            using (var p = ChoCSVReader.LoadText(text)
+                .WithFirstLineHeader()
+                )
+            {
+                using (var w = new ChoJSONWriter(sb))
+                    w.Write(p);
+            }
+
+            Console.WriteLine(sb.ToString());
+
+            return Ok(fileName);
+        }
+    }
+}
--- Projekat/Projekat/Ostalo/RadSaFajlovima.cs
+++ Projekat/Projekat/Ostalo/RadSaFajlovima.cs
@@ -1,11 +1,19 @@
-﻿namespace Projekat.Ostalo
+﻿using System.Linq;
+using CsvHelper;
+using System.Globalization;
+using System.IO;
+using System.Data;
+using Newtonsoft.Json;
+using CsvHelper.Configuration;
+
+namespace Projekat.Ostalo
 {
     public class RadSaFajlovima
     {
         public static bool ProveriAkoJeCsvFajl(IFormFile fajl)
         {
             var extension = "." + fajl.FileName.Split('.')[fajl.FileName.Split('.').Length - 1];
-            return (extension == ".xlsx" || extension == ".xls" || extension == ".csv"); 
+            return (extension == ".xlsx" || extension == ".xls" || extension == ".csv");
         }
 
         public static async Task<bool> UpisiFajl(IFormFile fajl)
@@ -41,5 +49,72 @@
 
             return uspesnoCuvanjeFajla;
         }
+
+        public static DataTable UcitajFajl(string imeFajla)
+        {
+            var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\csvFajlovi\\" + imeFajla + ".csv");
+            using (var streamReader = new StreamReader(pathBuilt))
+            {
+
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    var records = new List<dynamic>();
+                    csvReader.Read();
+                    csvReader.ReadHeader();
+                    records.Add(csvReader.GetRecord<dynamic>());
+                    while (csvReader.Read())
+                    {
+                        records.Add(csvReader.GetRecord<dynamic>());
+                    }
+
+                    var json = JsonConvert.SerializeObject(records);
+                    DataTable dataTable = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
+                    return dataTable;
+                }
+            }
+
+            return null;
+        }
+        public static DataTable UcitajFajl(string imeFajla, int BrojRedova, int RedniBrojStrane)
+        {
+            
+            var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\csvFajlovi\\" + imeFajla + ".csv");
+            using (var reader = new StreamReader(pathBuilt))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                int trenutniRed = 0;
+                int pocetniRed = (RedniBrojStrane - 1) * BrojRedova;
+                int poslednjiRed = RedniBrojStrane * BrojRedova;
+                var records = new List<dynamic>();
+                string json;
+                DataTable dataTable;
+                csv.Read();
+                csv.ReadHeader();
+                records.Add(csv.GetRecord<dynamic>());
+
+                while (csv.Read())
+                {
+                    if (trenutniRed<=poslednjiRed && trenutniRed>=pocetniRed)
+                    {
+                        records.Add(csv.GetRecord<dynamic>());
+                    }
+                    else if (trenutniRed > poslednjiRed)
+                    {
+                        json = JsonConvert.SerializeObject(records);
+                        dataTable = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
+                        return dataTable;
+                    }
+                    trenutniRed++;
+                }
+
+                json = JsonConvert.SerializeObject(records);
+                dataTable = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
+
+                return dataTable;
+            }
+            return null;
+        }
+        
+
     }
 }
