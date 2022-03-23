849e3e5afd1d472086f155e7199a160e218b4847
Stefan Janicijevic <stefan.janicijevic>
2022-03-23 15:26:04 +0100

    Dodat kostur pocetne strane i strane za statistiku. #32

---
+++ Projekat/Projekat/Modeli/EpocheResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekat.Modeli
+{
+    public class EpocheResultModel
+    {
+        public int ID { get; set; }
+        public int epocheNumber { get; set; }
+        public double Loss { get; set; }    
+        public double Precision { get; set; }    
+    }
+}
