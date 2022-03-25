e30eb15bb196bb87cced85358cc2ffe1b1c52da4
jelena.petrovic4 <jelena.petrovic4@pmf.kg.ac.rs>
2022-03-25 14:16:37 +0100

    dodato slanje podataka za crtanje grafika u funkciju koja vraca statistiku #35

--- Projekat/MachineLearning/statistics.py
+++ Projekat/MachineLearning/statistics.py
@@ -30,12 +30,24 @@ def getStats(data):
         recnik["broj_praznih_polja"]=broj_redova-opis["count"]
         if(column_name in kategorijske_kolone):
             #ubacivacemo u categorical columns
+            grafik=dict(data[column_name].value_counts().head(6))
+            if(len(grafik.keys())>4):
+                #dodaje se others kolona
+                naziv="others("+str(opis["unique"]-len(grafik.keys()))+")"
+                broj=broj_redova-sum(grafik.values())
+                grafik[naziv]=broj
+            #grafik=column_chart
             recnik["broj_jedinstvenih_polja"]=opis["unique"]
             recnik["najcesca_vrednost"]=opis["top"]
             recnik["najveci_broj_ponavljanja"]=opis["freq"]
+            recnik["column_chart_data"]=grafik
             analiza["kategoricke_kolone"].append(recnik)
+            
         else:
             #ubacivacemo u numerical columns
+            s=data[column_name]
+            s=pd.cut(s, 7,ordered=True).astype(str)
+            #grafik=histogram
             recnik["prosek"]=opis["mean"]
             recnik["standardna_devijacija"]=opis["std"]
             recnik["minimum"]=opis["min"]
@@ -44,6 +56,7 @@ def getStats(data):
             recnik["treci_kvartal"]=opis["75%"]
             recnik["maximum"]=opis["max"]
             recnik["broj_autlajera"]=sum(np.abs(stats.zscore(data[column_name])) > 3)
+            recnik["column_chart_data"]=dict(s.value_counts())
             analiza["numericke_kolone"].append(recnik) 
 
     return json.dumps(analiza,cls=NpEncoder,indent=4)
