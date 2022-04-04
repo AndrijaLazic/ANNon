9de3aa36eaba8c9a1712d68653fc16653bde5052
andrija.lazic <lazic.andrija223@gmail.com>
2022-04-04 07:38:25 +0200

    Modifikovano cuvanje fajlova -od sada cuvanje fajlova ne zavisi od lokacija API-a i Neur.Mreze

--- Projekat/Projekat/Clients/MachineLearningClient.cs
+++ Projekat/Projekat/Clients/MachineLearningClient.cs
@@ -43,9 +43,9 @@ namespace Projekat.Clients
         } 
         public async Task<string> sendData(string model)
         {
-            Console.WriteLine(model);
             var content = new StringContent(model,Encoding.UTF8, "application/json");
             HttpResponseMessage response = await _client.PostAsync(_configuration.GetSection("ML_Server_Config:http").Value + _configuration.GetSection("ML_Server_Config:host").Value + ":" + _configuration.GetSection("ML_Server_Config:port").Value + "/" + "send", content);
+
             var result = await response.Content.ReadAsStringAsync();
             return result;
         }
--- Projekat/Projekat/Controllers/SessionControler.cs
+++ Projekat/Projekat/Controllers/SessionControler.cs
@@ -21,87 +21,31 @@ namespace Projekat.Controllers
             _iCustomClient = iCustomClient;
         }
         [HttpPost("upload")]
-        public async Task<ActionResult<string>> uploadData(IFormFile file)
+        public async Task<ActionResult<string>> uploadData(string poruka)
         {
-            try
-            {
-
-                if (file.Length > 0)
-                {
-                    if (RadSaFajlovima.ProveriAkoJeCsvFajl(file))
-                    {
-                        if (await RadSaFajlovima.UpisiFajl(file))
-                        {
-                            var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\csvFajlovi\\" + file.FileName);
-                            DataModel dataModel = new DataModel();
-                            dataModel.FileName = file.FileName;
-                            dataModel.Putanja = pathBuilt;
-                            var jsonObject = JsonConvert.SerializeObject(dataModel);
-                            var answer = await _iCustomClient.sendData(jsonObject);
-                            
-                            string statistic = JsonConvert.DeserializeObject<String>(answer);
-                            
-                            return Ok(statistic);
-                        }
-                        return BadRequest("Greska pri upisivanju fajla");
-                    }
-                    return BadRequest("Fajl nije csv");
-
-                }
-                else
-                    return BadRequest("Fajl ne postoji");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            //try
+            //{
+            //    string pom = (RadSaFajlovima.UcitajFajl("637833769309922112", 20, 1)).Rows.Count.ToString();
+            //    foreach (DataRow red in (RadSaFajlovima.UcitajFajl("637833769309922112", 20, 1)).Rows)
+            //    {
+            //        Console.WriteLine(red[0]);
+            //    }
+            //    if (!string.IsNullOrEmpty(pom))
+            //    {
+            //        return Ok(pom);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    if (ex.Message.Contains("Could not find file"))
+            //        return BadRequest("Dati fajl ne postoji");
 
 
-            try
-            {
-                string pom = (RadSaFajlovima.UcitajFajl("637833769309922112",20,1)).Rows.Count.ToString();
-                foreach(DataRow red in (RadSaFajlovima.UcitajFajl("637833769309922112", 20, 1)).Rows){
-                    Console.WriteLine(red[0]);
-                }
-                if (!string.IsNullOrEmpty(pom))
-                {
-                    return Ok(pom);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                if (ex.Message.Contains("Could not find file"))
-                    return BadRequest("Dati fajl ne postoji");
-                
-                
-                return BadRequest("Doslo je do greske pri ucitavanju fajla");
-            }
-            
-            if (file.Length == 0)
-            {
-                return BadRequest("bad");
-            }
-            string fileName = file.FileName;
-            
-            await using var stream = file.OpenReadStream();
+            //    return BadRequest("Doslo je do greske pri ucitavanju fajla");
+            //}
 
-
-            var reader = new StreamReader(stream);
-            var text = await reader.ReadToEndAsync();
-
-            StringBuilder sb = new StringBuilder();
-            using (var p = ChoCSVReader.LoadText(text)
-                .WithFirstLineHeader()
-                )
-            {
-                using (var w = new ChoJSONWriter(sb))
-                    w.Write(p);
-            }
-
-            Console.WriteLine(sb.ToString());
-
-            return Ok(fileName);
+            return Ok(poruka);
         }
     }
 }
