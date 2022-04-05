63c5388e46d03b2b6237137d3f921e7a31d80a4a
jelena.petrovic4 <jelena.petrovic4@pmf.kg.ac.rs>
2022-04-05 20:39:40 +0200

    izmenjena statistika malo #35

--- Projekat/MachineLearning/statistics.py
+++ Projekat/MachineLearning/statistics.py
@@ -3,6 +3,20 @@ import pandas as pd
 import json
 from scipy import stats
 
+def determine_variable_types1(data):
+    nunique = data.nunique()
+    count=len(data)
+    dtypes = data.dtypes
+    categorical = []
+    numerical = []
+    dnu = dict(nunique)
+    for key in dnu:
+        if nunique[key] <= 20 and count>50:
+            categorical.append(key)
+        elif (dtypes[key] != object):
+            numerical.append(key)
+    
+    return (categorical, numerical)
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -21,7 +35,7 @@ def getStats(data):
     analiza["kategoricke_kolone"]=[]
 
     broj_redova=data.shape[0]
-    kategorijske_kolone=data.select_dtypes(include=["object"]).columns.values
+    kategorijske_kolone,_=determine_variable_types1(data)
 
     for column_name in data.columns:
         recnik=dict()
@@ -30,9 +44,11 @@ def getStats(data):
         recnik["broj_praznih_polja"]=broj_redova-opis["count"]
         if(column_name in kategorijske_kolone):
             #ubacivacemo u categorical columns
-            grafik=dict(data[column_name].value_counts().head(6))
-            if(len(grafik.keys())>4):
-                #dodaje se others kolona
+            jedinstvene=data[column_name].value_counts()
+            if(len(jedinstvene)<=7):
+                grafik=dict(jedinstvene.head(7))
+            else:
+                grafik=dict(jedinstvene.head(6))
                 naziv="others("+str(opis["unique"]-len(grafik.keys()))+")"
                 broj=broj_redova-sum(grafik.values())
                 grafik[naziv]=broj
@@ -46,10 +62,11 @@ def getStats(data):
         else:
             #ubacivacemo u numerical columns
             s=data[column_name]
+            s.rolling(window=20).mean().plot()
             s=pd.cut(s, 7,ordered=True).astype(str)
             #grafik=histogram
-            recnik["prosek"]=opis["mean"]
-            recnik["standardna_devijacija"]=opis["std"]
+            recnik["prosek"]=round(opis["mean"],2)
+            recnik["standardna_devijacija"]=round(opis["std"],2)
             recnik["minimum"]=opis["min"]
             recnik["prvi_kvartal"]=opis["25%"]
             recnik["drugi_kvartal"]=opis["50%"]
@@ -60,4 +77,8 @@ def getStats(data):
             analiza["numericke_kolone"].append(recnik) 
 
     return json.dumps(analiza,cls=NpEncoder,indent=4)
+
+data=pd.read_csv("diamonds.csv")
+print(data["Price"].rolling(window=100).mean())
+print(getStats(data))
 
