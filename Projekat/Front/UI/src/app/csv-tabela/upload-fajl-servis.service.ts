72eed4eda79e65e89fa426df314e87147d6348ec
andrija.lazic <lazic.andrija223@gmail.com>
2022-05-02 22:07:17 +0200

    Fix za slanje csv fajla i fix za hendlovanje gresaka

--- Projekat/Front/UI/src/app/csv-tabela/upload-fajl-servis.service.ts
+++ Projekat/Front/UI/src/app/csv-tabela/upload-fajl-servis.service.ts
@@ -6,49 +6,21 @@ import { catchError, map, Observable, of } from 'rxjs';
 import { default as Konfiguracija } from '../../../KonfiguracioniFajl.json';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UploadFajlServisService {
-
-  constructor(private http:HttpClient,private toastr:ToastrService, private spinner: NgxSpinnerService) { }
-  procenatUploada=0;
-  public uploadFile(file:any): Observable<UploadResponse> {
-    const form = new FormData();
-    //this.setSession();
-    
-    
-    form.append("uploadedFile",file);
-    form.append("userID",sessionStorage.getItem("userId"));
-    const baseURL=Konfiguracija.KonfiguracijaServera.osnovniURL
-    const api = baseURL+"api/MachineLearning/uploadFile";
-    
-    const req = new HttpRequest('POST', api, form, { reportProgress: true });
-      return this.http.request(req).pipe(
-        map((res: HttpEvent<any>) => {
-          if (res.type === HttpEventType.Response) {
-            const responseFromBackend = res.body;
-            return {
-              body: responseFromBackend,
-              status: UploadStatus.UPLOADED
-            };
-          } else if (res.type === HttpEventType.UploadProgress) {
-            /** Predstavlja procenat upload-a: */
-            const uploadProgress = +Math.round((100 * res.loaded) / res.total);
-            this.procenatUploada=uploadProgress;
-            return {
-              status: UploadStatus.IN_PROGRESS,
-              progress: uploadProgress
-            };
-
-          }
-        }),
-        catchError(er => {
-          this.spinner.hide("Spiner2");
-          this.toastr.error("Neuspešna konekcija sa serverom!","Greška")
-          console.log(er);
-          return of({ status: UploadStatus.ERROR, body: er });
-        })
-      );
+  private baseUrl = Konfiguracija.KonfiguracijaServera.osnovniURL;
+  constructor(private http: HttpClient) { }
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+    formData.append("uploadedFile",file);
+    formData.append("userID",sessionStorage.getItem("userId"));
+    const req = new HttpRequest('POST',this.baseUrl+"api/MachineLearning/uploadFile", formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+    return this.http.request(req);
   }
 }
