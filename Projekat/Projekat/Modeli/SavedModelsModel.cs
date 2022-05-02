29536b049f6c077eb99acb51ca282f6e8899462d
jelena.petrovic4 <jelena.petrovic4@pmf.kg.ac.rs>
2022-05-02 15:58:45 +0200

    SavedModelsModel dodat

---
+++ Projekat/Projekat/Modeli/SavedModelsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekat.Modeli
+{
+    public class SavedModelsModel
+    {
+        public int UserID { get; set; }
+        public string ModelID { get; set; }
+        public DateTime DateSaved { get; set; }
+        public string ModelName { get; set; }
+    }
+}
