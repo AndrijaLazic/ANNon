a43efcc8e9779aa08a8dcffd95eff08724f169dc
andrija.lazic <lazic.andrija223@gmail.com>
2022-06-01 23:09:27 +0200

    Rad na servisu za brisanje fajlova

---
+++ Projekat/Projekat/Servisi/TimerWorkerService.cs
@@ -0,0 +1,28 @@
+﻿namespace Projekat.Servisi
+{
+    public class TimerWorkerService : IHostedService,IDisposable
+    {
+        private readonly ILogger _logger;
+        private Timer timer;
+        public TimerWorkerService(ILogger<TimerWorkerService> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
