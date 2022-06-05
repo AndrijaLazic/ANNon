af739a846fb88e6dc25be6c1c2e97e159dd1de75
Jelena Petrovic <jelenapetrovic.7119@gmail.com>
2022-06-06 01:29:41 +0200

    centrirana tabela modela

---
+++ Projekat/Projekat/Migrations/20220605225914_pk.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekat.Migrations
+{
+    public partial class pk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
